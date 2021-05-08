8d14386da9f9b6b8b072bf9d7a793cb09f142372
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-05-08 17:44:19 -0300

    style: bootstrap and global style added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Olá Mundo!</h1>
+        <h1 className="text-primary">Olá Mundo!</h1>
       </header>
     </div>
   );
